68869a8da05526885508d5be4024120b5d2c586a
Made-of-Clay <adam.m.leis@gmail.com>
2017-10-04 11:01:54 -0400

    photos show; WIP show photos in dialog

--- src/Portfolio.component.vue
+++ src/Portfolio.component.vue
@@ -12,12 +12,31 @@
         <p>large image here</p>
         <p>thumbnails here</p>
 
-        <div v-for="entry in info" style="border-bottom:1px solid">
+        <div v-for="(entry, index) in info" style="border-bottom:1px solid">
             Title: {{entry.title}} <br>
             Desc: <span v-html="entry.desc"></span><br>
             <a :href="entry.link">Link</a> <br>
-            <img :src="entry.src" :alt="entry.title">
+
+            <div id="preview-image">
+                <img :src="entry.src" 
+                    :id="buildID(index)"
+                    :alt="entry.title"
+                    @click="showImage(index)">
+            </div>
         </div>
+
+        <md-dialog ref="dialog"
+            md-open-from="preview-image"
+            md-close-to="preview-image">
+            
+            <md-dialog-title>{{selected.title}}</md-dialog-title>
+
+            <md-dialog-content>
+                <img id="full-image"
+                    :src="selected.src"
+                    :alt="selected.title">
+            </md-dialog-content>
+        </md-dialog>
     </main>
 </template>
 
@@ -29,8 +48,42 @@ export default {
     data() {
         return {
             info,
-            foo: 'bar'
+            selectedIndex: -1
         };
     },
+    computed: {
+        selected() {
+            let fakeSelected = {
+                title: '',
+                desc: '',
+                link: '',
+                src: ''
+            };
+            return (this.selectedIndex >= 0) 
+                ? this.info[selectedIndex] 
+                : fakeSelected;
+        },
+    },
+    methods: {
+        buildID(index) {
+            if (isUndefined(index)) return '';
+
+            return this.info[index].title.toLowerCase().replace(/ /g, '_');
+        },
+        showImage(index) {
+            this.selectedIndex = index;
+            this.$refs.dialog.open();
+        },
+    },
 };
+
+function isUndefined(value) {
+    return typeof value === 'undefined';
+}
+function objectEmpty(obj) {
+    return (Object.keys(obj)).length === 0;
+}
+function goodIndex(value) {
+    return value > -1;
+}
 </script>
