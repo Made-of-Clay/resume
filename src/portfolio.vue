9ca1cf8b1840d4527bd7218f55b46bef0cdeae4e
Made-of-Clay <adam.m.leis@gmail.com>
2019-03-13 21:28:52 -0400

    add rough markup for portfolio w/ notes

--- src/portfolio.vue
+++ src/portfolio.vue
@@ -1,7 +1,24 @@
 <template>
-    <div>
+    <article>
         Portfolio component: foo {{foo}}
-    </div>
+        <h1>Portfolio</h1>
+
+        <div>chevron arrows and drop down (select) of project names</div>
+        <!-- <project-select /> -->
+
+        <section class="portfolio__projText">
+            <h2>Project names</h2>
+            <p>Project Desc/caption</p>
+            <p>(desc scrolls vert if needed</p>
+        </section>
+
+        <section class="portfolio__projPics">
+            carousel? no timing
+            images saved to media lib w/ cat: "Whatever" under parent "Portfolio"
+            click big thumb to expand (dialog?)
+            /portfolio/:project/:thumb-n/
+        </section>
+    </article>
 </template>
 
 <script>
