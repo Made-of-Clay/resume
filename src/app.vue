55f2b65d0b57285e61ce7807cc33c0a5045ec17c
Adam <adam.m.leis@gmail.com>
2020-03-30 12:38:49 -0400

    basic shell of site built

--- src/app.vue
+++ src/app.vue
@@ -2,12 +2,15 @@
     <v-app>
         <!-- my red: #da1e33 -->
         <!-- my gray: #626366 -->
+        <!-- TODO replace src w/ my resume black background image tile (and keep always?) -->
+        <!-- might also do pic w/ red/gray? gradient then change when scrolled... dunno yet -->
         <v-app-bar
             color="#da1e33"
             src="https://picsum.photos/1920/1080?random"
             app dark
             shrink-on-scroll
             fade-img-on-scroll
+            clipped-left
         >
             <template v-slot:img="{ props }">
                 <v-img
@@ -16,6 +19,7 @@
                 />
             </template>
 
+            <v-app-bar-nav-icon @click="toggleNavDrawer" />
             <v-toolbar-title>
                 Adam Leis
             </v-toolbar-title>
@@ -31,8 +35,18 @@
             </v-btn>
         </v-app-bar>
 
-        <v-sheet class="overflow-y-auto">
-            <v-container style="margin-top:140px">
+        <v-navigation-drawer v-model="drawer" app clipped>
+            <v-list dense>
+                <v-list-item>
+                    Section w/ icon jumps to section on page
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-content>
+            <v-container>
+                <a href="https://google.com">Testing a link</a>
+
                 <ul>
                     <li
                         v-for="(section, i) in sections"
@@ -53,7 +67,7 @@
                 <h5>Filler content</h5>
                 <lipsum />
             </v-container>
-        </v-sheet>
+        </v-content>
     </v-app>
 </template>
 
@@ -62,6 +76,8 @@ import lipsum from './lipsum';
 import siteSection from './site-section';
 
 export default {
+    name: 'AdamLeis',
+
     components: {
         lipsum,
         siteSection,
@@ -77,15 +93,22 @@ export default {
             'education',
             'social links',
         ],
+        drawer: false,
     }),
 
     created() {
     },
 
     methods: {
+        toggleNavDrawer() {
+            this.drawer = !this.drawer;
+        },
     },
 };
 </script>
 
 <style lang="scss">
+a {
+    color: #da1e33;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,15 @@ import app from './app';
 Vue.use(Vuetify);
 
 const vuetify = new Vuetify({
-    // options
+    theme: {
+        themes: {
+            light: {
+                primary: '#da1e33',
+                secondary: '#626366',
+                accent: '#af182a'
+            },
+        },
+    }
 });
 
 new Vue({
