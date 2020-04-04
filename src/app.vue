c531d13ad0511ae73383bb286a701880d3b46ce6
Adam <adam.m.leis@gmail.com>
2020-04-04 01:41:57 -0400

    add resume download

--- src/app.vue
+++ src/app.vue
@@ -5,10 +5,22 @@
         <!-- TODO replace src w/ my resume black background image tile (and keep always?) -->
         <!-- might also do pic w/ red/gray? gradient then change when scrolled... dunno yet -->
         <site-header />
-
         <site-nav-drawer />
-
         <site-content />
+        <v-tooltip top>
+            <template v-slot:activator="{on}">
+                <v-btn
+                    dark fab
+                    fixed bottom right
+                    color="primary"
+                    @click="downloadResume"
+                    v-on="on"
+                >
+                    <v-icon>mdi-download</v-icon>
+                </v-btn>
+            </template>
+            <span>Download PDF Resume</span>
+        </v-tooltip>
     </v-app>
 </template>
 
@@ -31,11 +43,25 @@ export default {
             this.$store.commit('openDrawer');
         }
     },
+
+    methods: {
+        downloadResume() {
+            window.open('/AdamLeis-resume.pdf');
+        },
+    },
 };
 </script>
 
 <style>
+/* @font-face {
+    font-family: 'bebasregular';
+    src: url('./assets/bebas-regular-webfont.woff2') format('woff2'),
+         url('./assets/bebas-regular-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+} */
 a[href] {
+    border-bottom: 1px solid;
     text-decoration: none;
     transition: all 0.25s;
 }
